23096f0bf93812f10e100017ae228a293f2a20d9
GTBoon72 <gtboon.nl@gmail.com>
2016-07-21 10:47:02 +0200

    changed vm creation to vm import

--- runner/artifacts/entrypoint.sh
+++ runner/artifacts/entrypoint.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-VM=raamkozijn
+VM=`ls packer*.ovf|cut -d'.' -f1`
 
 /sbin/rcvboxdrv setup
-VBoxManage createvm --name $VM --ostype "Windows7" --register
-VBoxManage storagectl $VM --name "IDE" --add ide  --bootable on --controller PIIX4
-#VBoxManage storagectl $VM --name "SATA Controller" --add sata  --bootable on
-#VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0
-VBoxManage natnetwork add --netname NATnetwork --network 10.0.3.0/24 --dhcp on
-VBoxManage modifyvm $VM --memory 2048 --vram 32 --cpus 1
-VBoxManage modifyvm $VM --nat-network1 NATnetwork
-VBoxManage modifyvm $VM --nic1 natnetwork
-VBoxManage modifyvm $VM --nictype1 82540EM
-VBoxManage modifyvm $VM --natnet1 NATnetwork
-VBoxManage modifyvm $VM --audio none
-VBoxManage modifyvm $VM --ioapic on
-#vboxmanage hostonlyif create
-#vboxmanage hostonlyif ipconfig "vboxnet0" --dhcp 
-#VBoxManage storageattach $VM --storagectl "SATA Controller" --type hdd --port 0 --medium /$VM.vmdk
-VBoxManage storageattach $VM --storagectl "IDE" --port 0 --device 0 --type hdd --medium /$VM.vmdk
+ln -s /usr/bin/dracut /usr/sbin/dracut
+VBoxManage import packer*.ovf
+VBoxManage modifyvm $VM --cpus 1
+VBoxManage modifyvm $VM --nestedpaging off
+VBoxManage modifyvm $VM --pae off
+VBoxManage modifyvm $VM --vtxux off
+VBoxManage modifyvm $VM --vtxvpid off
+VBoxManage modifyvm $VM --hwvirtex off
+VBoxManage modifyvm $VM --longmode off
+VBoxManage setproperty hwvirtexclusive off
 VBoxManage startvm --type gui $VM
 tail -F /var/log/vbox-install.log
